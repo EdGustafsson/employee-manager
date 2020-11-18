db65acc059d1acb4464b6dd606c5af8e003f1b6c
EdGustafsson <edlimmet@gmail.com>
2020-11-19 00:29:07 +0100

    Foundation

---
+++ EmployeeManager/Users/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeManager.Users
+{
+    public class User
+    {
+
+        public User(Guid id, string firstname, string lastname ,string password ,string address, bool admin)
+        {
+            Id = id;
+            FirstName = firstname;
+            LastName = lastname;
+            Password = password;
+            Address = address;
+            Admin = admin;
+        }
+
+        public Guid Id { get; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string Password { get; private set; }
+        public string Address { get; private set; }
+        public bool Admin { get; private set; }
+
+
+    }
+}
