f6e64fa82dba39b0c184e38c280cd96b9370577c
EdGustafsson <edlimmet@gmail.com>
2020-11-21 02:00:39 +0100

    UserUI enfore unique ID

--- UserUI/Program.cs
+++ UserUI/Program.cs
@@ -46,40 +46,59 @@ namespace UserUI
 
             while (true)
             {
+                Console.WriteLine("\nAccount information");
                 Console.WriteLine("Id = " + activeUser.Id);
                 Console.WriteLine("Name = " + activeUser.Name);
                 Console.WriteLine("Password = " + activeUser.Password);
                 Console.WriteLine("Address = " + activeUser.Address);
 
                 Console.WriteLine("\nEnter which item you would like to change");
-                Console.WriteLine("'id' for Id");
-                Console.WriteLine("'name' for Id");
-                Console.WriteLine("'password' for Id");
-                Console.WriteLine("'address' for Id");
+                Console.WriteLine("*In lowercase only");
 
                 string userChoice = Console.ReadLine();
 
-                switch (userChoice)
+
+
+
+                if (userChoice == "id")
                 {
-                    case "id":
+                    while (true)
+                    {
                         Console.Write("\nEnter new id:");
-                        activeUser.Id = Console.ReadLine();
-                        break;
-                    case "name":
-                        Console.Write("\nEnter new name:");
-                        activeUser.Name = Console.ReadLine();
-                        break;
-                    case "password":
-                        Console.Write("\nEnter new password:");
-                        activeUser.Password = Console.ReadLine();
-                        break;
-                    case "address":
-                        Console.Write("\nEnter new address:");
-                        activeUser.Address = Console.ReadLine();
-                        break;
+                        string id = Console.ReadLine();
+
+                        if (_userRepository.UserExist(id))
+                        {
+                            Console.WriteLine("\nA user with that ID already exists");
+                        }
+                        else
+                        {
+
+                            activeUser.Id = id;
+                            break;
+                        }
+                    }
+
                 }
+                else
+                {
+                    switch (userChoice)
+                    {
 
-
+                        case "name":
+                            Console.Write("\nEnter new name:");
+                            activeUser.Name = Console.ReadLine();
+                            break;
+                        case "password":
+                            Console.Write("\nEnter new password:");
+                            activeUser.Password = Console.ReadLine();
+                            break;
+                        case "address":
+                            Console.Write("\nEnter new address:");
+                            activeUser.Address = Console.ReadLine();
+                            break;
+                    }
+                }
 
                 _userRepository.Save();
 
